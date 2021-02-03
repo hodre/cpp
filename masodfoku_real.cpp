ccdaf5e264537164df011b73b6cb9871bc71cd05
hodre <vargadsebestyen@gmail.com>
2021-02-03 15:39:08 +0100

    masodfoku valos szamokra added

---
+++ masodfoku_real.cpp
@@ -0,0 +1,38 @@
+/*
+Varga Sebestyén
+2021.02.03.
+másodfokú egyenletmegoldás valós számok halmazában
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double a,b,c;
+    double x1,x2;
+    double D; //diszkrimináns
+    
+    cout << "Adja meg a másodfokú egyenlet 3 együtthatóját!\n";
+    cout << "a=";
+    cin >> a;
+    cout << "b=";
+    cin >> b;
+    cout << "c=";
+    cin >> c;
+    
+    D=(b*b-4*a*c);
+    if(D<0)
+    {
+     cout <<"Nincs valós megoldása az egyenletnek...:(";
+     return 0;
+    } else
+    {
+        x1=(-b+sqrt(D))/2*a;
+        x2=(-b-sqrt(D))/2*a;
+        cout << "x1=" << x1 << endl;
+        cout << "x2=" << x2 << endl;
+        return 0;
+    }   
+}
